4e0977a33513b25c6862dada8db1706bb5ca3a17
beeforham <47697782+beeforham@users.noreply.github.com>
2020-04-11 09:51:38 -0500

    Added chat event

--- src/anarchy/events.java
+++ src/anarchy/events.java
@@ -1,8 +1,10 @@
 package anarchy;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class events implements Listener {
@@ -12,6 +14,16 @@ public class events implements Listener {
         Player p = e.getPlayer();
         p.sendMessage(ChatColor.GREEN + "This is an anarchy server, there are no rules so do what you want!");
         p.sendMessage(ChatColor.RED + "Damaging the servers integrity (e.g: lag machines) may be frowned upon and result in the only softban possibility!");
+
+    }
+
+    public void onChat(AsyncPlayerChatEvent e) {
+        e.setCancelled(true);
+        String p = e.getPlayer().getDisplayName();
+        String msg = e.getMessage();
+
+        Bukkit.getServer().broadcastMessage(ChatColor.GRAY + p + msg);
+
 
     }
 
