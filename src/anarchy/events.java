7c6e6e0ec8cdf7dd5b976ef99c14aea980eaaf30
beeforham <47697782+beeforham@users.noreply.github.com>
2020-04-11 09:59:32 -0500

    Added first join logic + More chat events

--- src/anarchy/events.java
+++ src/anarchy/events.java
@@ -12,6 +12,18 @@ public class events implements Listener {
     public void onPlayerJoin (PlayerJoinEvent e) {
 
         Player p = e.getPlayer();
+        boolean hasjoined = p.hasPlayedBefore();
+
+        if(!hasjoined) {
+
+            e.setJoinMessage(ChatColor.LIGHT_PURPLE + "Joined >> " + ChatColor.WHITE + p.getName() + " has joined for the first time!");
+
+        }else {
+
+            e.setJoinMessage(ChatColor.GREEN + p.getName() + " logged on!");
+
+        }
+
         p.sendMessage(ChatColor.GREEN + "This is an anarchy server, there are no rules so do what you want!");
         p.sendMessage(ChatColor.RED + "Damaging the servers integrity (e.g: lag machines) may be frowned upon and result in the only softban possibility!");
 
@@ -24,7 +36,8 @@ public class events implements Listener {
 
         Bukkit.getServer().broadcastMessage(ChatColor.GRAY + p + msg);
 
-
     }
+
+
 
 }
